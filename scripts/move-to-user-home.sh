abe61b58c25e9c55e4412bcfa5d430a607f0536e
Antonio <antonio@cus.ro>
2018-12-31 06:53:31 +0200

    Fixed move-to-user-home script

--- scripts/move-to-user-home.sh
+++ scripts/move-to-user-home.sh
@@ -5,13 +5,18 @@
 #======================================================================================================================
 
 # Get the full path to the current script
-scriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+#scriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # The current script is in the "scripts" folder, in the setup folder, so get the full path to the setup folder
-setupFolder=$(dirname ${scriptFolder})
+setupFolder=$(dirname ${DIR})
+echo "Setup Folder: ${setupFolder}"
 
 # Get the folder name of the setup folder (should be "setup", but it may have changed)
 folderName=$(basename ${setupFolder})
+echo "Folder Name: ${folderName}"
 
-mv setupFolder /home/$1/
-chown -R $1 /home/$1/$folderName
+mv ${setupFolder} /home/$1/
+chown -R $1 /home/$1/${folderName}
